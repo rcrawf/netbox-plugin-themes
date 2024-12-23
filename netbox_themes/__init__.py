a8c5bb207a1d1c89af9c193e07ec63133d0cbbdc
rob <rob@example.com>
2024-12-23 00:43:21 +0000

    bump version, add sample theme

--- netbox_themes/__init__.py
+++ netbox_themes/__init__.py
@@ -7,7 +7,7 @@ class NetBoxThemes(PluginConfig):
     name = 'netbox_themes'
     verbose_name = 'NetBox Themes'
     description = 'Manage CSS themes for NetBox'
-    version = '0.3.0'
+    version = '0.3.1'
     base_url = 'themes'
 
     def ready(self):
